ed3219dc9a40424da175bc40f9e9a9a9b68fe02a
Jawad <jawadal829@gmail.com>
2025-03-10 20:40:20 +0500

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,59 @@
 import { useState } from "react";
 import "./App.css";
-import { useEffect } from "react";
-
 function App() {
-  const [step, setStep] = useState(1);
-  useEffect(() => {
-    if (step <= 6) {
-      setTimeout(() => {
-        setStep(step + 1);
-      }, 3000);
-    } else {
-      setStep(1);
-    }
-  }, [setStep, step]);
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
       <header className="header flex">
         <h1 className="logo flex">
           S <div className="line"></div> D
         </h1>
-        <div className="menu-bars flex col">
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
+        <div
+          className="menu-bars flex col"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <div
+            className="bar"
+            style={{
+              position: showMenu ? "absolute" : "",
+              rotate: showMenu ? "45deg" : "0deg",
+            }}
+          ></div>
+          <div
+            className="bar"
+            style={{
+              transform: showMenu ? "translateX(-40px)" : "translateX(0)",
+            }}
+          ></div>
+          <div
+            className="bar"
+            style={{
+              position: showMenu ? "absolute" : "",
+              rotate: showMenu ? "-45deg" : "0deg",
+            }}
+          ></div>
+        </div>
+        <div
+          className="main-menu flex"
+          style={{
+            transform: showMenu ? "translateY(0)" : "translateY(-100%)",
+          }}
+        >
+          {showMenu && (
+            <div className="nav-menu flex col">
+              <a href="/" className="flex">
+                <span>01.</span>About
+              </a>
+              <a href="/" className="flex">
+                <span>02.</span>Services
+              </a>
+
+              <a href="/" className="flex">
+                <span>03.</span>Contact
+              </a>
+            </div>
+          )}
         </div>
       </header>
       <div className="main-container flex col">
@@ -46,15 +77,8 @@ function App() {
         <div className="about highlights flex col">
           <div className="line flex">
             <h2>02.</h2>
-            <h1>highlights</h1>
+            <h1>EXplore my projects</h1>
             <button className="flex">my projects</button>
-          </div>
-          <div className="main-highlights flex col">
-            <img
-              src={`/` + step + `.png`} // Shortened ternary logic
-              alt=""
-              className="slide-animation"
-            />
           </div>
         </div>
       </div>
